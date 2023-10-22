0c9c117ecfa4741cc88c682048de9f638ca13322
Rivaldi0097 <rivaldi1997@hotmail.com>
2023-10-23 03:21:06 +0800

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
     origin: '*',
-    allowedHeaders: ['Content-Type']
+    allowedHeaders: ['Content-Type', 'Access-Control-Allow-Headers']
 }))
 
 app.use(morgan('dev'));
